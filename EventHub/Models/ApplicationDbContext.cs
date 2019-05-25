041e2ae735cee713319e9619ad0d02753cda4c31
Kamrul Hasan <Kamrul Hasan>
2019-05-26 03:12:29 +0600

    Split application db context

---
+++ EventHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EventHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("EventHubConString", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
