f89ad3216852ddb3db0dd9bb9c845823c4cbeacf
Kamrul Hasan <Kamrul Hasan>
2019-05-27 04:30:09 +0600

    Add Event Model

---
+++ EventHub/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace EventHub.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ EventHub/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventHub.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Organizer { get; set; }
+
+        [Required]
+        public string OrganizerId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Vanue { get; set; }
+
+        public Category Category { get; set; }
+
+        [Required]
+        public byte CategoryId { get; set; }
+
+
+    }
+}
