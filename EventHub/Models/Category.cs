5ef904db82d693db3092b67a87a645ebe09186d8
Kamrul Hasan <Kamrul Hasan>
2019-05-27 04:31:04 +0600

    Add Category Model

--- EventHub/Models/Category.cs
+++ EventHub/Models/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace EventHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventHub.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
     }
 }
