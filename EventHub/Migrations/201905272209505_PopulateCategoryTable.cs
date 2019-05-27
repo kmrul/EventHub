c6d6bbe9a73c04e2bc68de8705b636e896955c7b
Kamrul Hasan <Kamrul Hasan>
2019-05-28 04:12:20 +0600

    Populate Category Sql data

---
+++ EventHub/Migrations/201905272209505_PopulateCategoryTable.cs
@@ -0,0 +1,20 @@
+namespace EventHub.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Categories (Id, Name) VALUES ('1','Educational')");
+            Sql("INSERT INTO Categories (Id, Name) VALUES ('2','Social')");
+            Sql("INSERT INTO Categories (Id, Name) VALUES ('3','Meetup')");
+            Sql("INSERT INTO Categories (Id, Name) VALUES ('4','Non Profit')");
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM Categories WHERE Id IN (1,2,3,4)");
+        }
+    }
+}
