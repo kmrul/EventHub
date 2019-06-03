81182862939ca9f29f1d01eb0310620796c697af
Kamrul Hasan <Kamrul Hasan>
2019-06-03 17:15:29 +0600

    Home Event Page Data view controller

--- EventHub/Controllers/HomeController.cs
+++ EventHub/Controllers/HomeController.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EventHub.Models;
+using EventHub.ViewModels;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace EventHub.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingEvents = _context.Events
+                .Include(e => e.Organizer)
+                .Include(e => e.Category)
+                .Where(e => e.DateTime > DateTime.Now);
+
+            var viewModel = new EventsViewModel
+            {
+                UpcommingEvents = upcommingEvents,
+                ShowingActions = User.Identity.IsAuthenticated,
+                Heading = "Show Upcomming Events"
+            };
+
+            return View("Events",viewModel);
         }
 
         public ActionResult About()
---
+++ EventHub/ViewModels/EventsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EventHub.Models;
+using System.Collections.Generic;
+
+namespace EventHub.ViewModels
+{
+    public class EventsViewModel
+    {
+        public IEnumerable<Event> UpcommingEvents { get; set; }
+
+        public bool ShowingActions { get; set; }
+
+        public string Heading { get; set; }
+    }
+}
