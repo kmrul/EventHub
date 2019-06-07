e03c6a9eaef86436b4f2c68447b8140220af48ec
Kamrul <khasancsit@gmail.com>
2019-06-07 10:10:24 +0600

    Add going button action

---
+++ EventHub/Controllers/AttendancesController.cs
@@ -0,0 +1,43 @@
+﻿using EventHub.Dtos;
+using EventHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.EventId == dto.EventId))
+                return BadRequest("Attendance already exits");
+
+            var attendance = new Attendance
+            {
+                EventId = dto.EventId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- EventHub/Global.asax.cs
+++ EventHub/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Routing;
+using EventHub.App_Start;
 
 namespace EventHub
 {
@@ -12,6 +15,7 @@ namespace EventHub
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
