be6bdb9e3b695ba7b267521b552bee67da48458b
Kamrul <khasancsit@gmail.com>
2019-06-15 01:23:04 +0600

    delete api implementation

---
+++ EventHub/Controllers/Api/EventsController.cs
@@ -0,0 +1,35 @@
+﻿using EventHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventHub.Controllers.Api
+{
+    [Authorize]
+    public class EventsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var evnt = _context.Events.Single(g => g.Id == id && g.OrganizerId == userId);
+            evnt.IsCanceled = true;
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
+    }
+}
